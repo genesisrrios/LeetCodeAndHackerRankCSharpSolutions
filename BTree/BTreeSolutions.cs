6f2b5f31a88774ab9288153b8a4de84171451224
Genesis Rivera <genesisrrios@gmail.com>
2021-07-08 08:50:57 -0400

    Add project files.

---
+++ BTree/BTreeSolutions.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTree
+{
+    public class BTreeSolutions
+    {
+        private IList<int> TreeNodeList = new List<int>();
+        private IList<IList<int>> LevelOrderNodeList = new List<IList<int>>();
+        private bool IsFirstNode = true;
+        public IList<IList<int>> LevelOrderTraversal(TreeNode root)
+        {
+            var nodes = new List<int>();
+            if (root != default)
+            {
+                if (IsFirstNode)
+                {
+                    IsFirstNode = false;
+                    LevelOrderNodeList.Add(new int[] { root.val });
+                }
+
+                if (root.left != default)
+                {
+                    nodes.Add(root.left.val);
+                }
+                if (root.right != default)
+                {
+                    nodes.Add(root.right.val);
+                }
+                if (root.right != default || root.left != default)
+                {
+                    LevelOrderNodeList.Add(nodes);
+                }
+                if (root.left != default)
+                {
+                    LevelOrderTraversal(root.left);
+                }
+                if (root.right != default)
+                {
+                    LevelOrderTraversal(root.right);
+                }
+            }
+            return LevelOrderNodeList;
+        }
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            if (root != default)
+            {
+                TreeNodeList.Add(root.val);
+                if (root.left != default)
+                {
+                    PreorderTraversal(root.left);
+                }
+                if (root.right != default)
+                {
+                    PreorderTraversal(root.right);
+                }
+            }
+            return TreeNodeList;
+        }
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            if (root != default)
+            {
+                if (root.left != default)
+                {
+                    InorderTraversal(root.left);
+                }
+                TreeNodeList.Add(root.val);
+                if (root.right != default)
+                {
+                    InorderTraversal(root.right);
+                }
+            }
+            return TreeNodeList;
+        }
+        public IList<int> PostOrderTraversal(TreeNode root)
+        {
+            if (root != default)
+            {
+                if (root.left != default)
+                {
+                    PostOrderTraversal(root.left);
+                }
+                if (root.right != default)
+                {
+                    PostOrderTraversal(root.right);
+                }
+                TreeNodeList.Add(root.val);
+            }
+            return TreeNodeList;
+        }
+    }
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = default, TreeNode right = default)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ BTree/SalesMerchant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTree
+{
+    public class SalesMerchant
+    {
+        public static int SockMerchant(int n, int[] ar)
+        {
+            var numberOfPairs = 0;
+            var distinctSock = ar.Distinct().ToArray();
+
+            for (int i = 0; i < distinctSock.Length; i++)
+            {
+                var count = ar.Where(x => x == distinctSock[i]).Count();
+
+                if (count == 2)
+                    numberOfPairs++;
+                else
+                {
+                    var number = count / 2;
+                    numberOfPairs = numberOfPairs + number;
+                }
+
+            }
+
+            return numberOfPairs;
+        }
+    }
+}
